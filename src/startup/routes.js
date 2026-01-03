5d73a857e55da158cc943b3a4198130d74519cb9
Eslam Amin <eslam.amin.610@gmail.com>
2026-01-03 15:34:48 +0200

    Fixed wildcard error

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -8,7 +8,7 @@ appRouter.use("/stores", storeRoutes);
 appRouter.use("/inventory", inventoryRoutes);
 
 // Not Found Route
-appRouter.all("*", (req, res, next) => {
+appRouter.use((req, res, next) => {
   console.log("No matching route for:", req.url); // Add logging
   next(ApiError.notFound("Route not found"));
 });
