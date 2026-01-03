b88d67da81a5b8120b123b9e77b77cfee923f0dc
Eslam Amin <eslam.amin.610@gmail.com>
2026-01-03 14:49:54 +0200

    Created app.js file

---
+++ src/startup/app.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const compression = require("compression");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const globalErrorMiddleware = require("../middlewares/globalError.middleware");
+const appRouter = require("./routes");
+
+module.exports = (app) => {
+  // Middlewares
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(cors());
+  app.options("*", cors());
+  app.use(compression());
+  app.use(helmet());
+  //Data Sanitization against XSS
+  app.use(xss());
+  //Prevent parameter pollution
+  app.use(hpp());
+
+  app.use(express.static(path.join(__dirname, "uploads")));
+
+  app.use("/api", appRouter);
+
+  // Global Error Handler
+  app.use(globalErrorMiddleware);
+};
