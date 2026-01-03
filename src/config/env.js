caa290641ea8ac9e0609e877175bdc7827f870dd
Eslam Amin <eslam.amin.610@gmail.com>
2026-01-03 14:44:43 +0200

    Created a env.js file to access files from .env

---
+++ src/config/env.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const env = Object.freeze({
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER || "postgres",
+  dbPass: process.env.DB_PASS || "password",
+  dbName: process.env.DB_NAME || "bookstore",
+  dbHost: process.env.DB_HOST || "localhost",
+  dbPort: process.env.DB_PORT || 5432,
+
+  nodeEnv: process.env.NODE_ENV
+});
+
+module.exports = { env };
