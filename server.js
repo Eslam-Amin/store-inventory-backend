46a2520045a27b1728c0028b0c9cf7b2abcb05a4
Eslam Amin <eslam.amin.610@gmail.com>
2026-01-03 14:44:22 +0200

    Created Our entry-point of the application

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { initDb } = require("./src/startup/db");
+const { env } = require("./src/config/env");
+
+// Express app
+const app = express();
+
+const server = require("http").createServer(app);
+const PORT = env.port || 8000;
+
+require("./src/startup/app")(app);
+
+// Initialize DB and Start Server
+initDb().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
